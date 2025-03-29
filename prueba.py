2989a554f7c77c18722ac32a8f86b6077f7baa16
Gotheru <gotheru@gmail.com>
2025-03-29 10:35:50 +0100

    Modificado para que use un entorno de pip y que torch se ejecute sobre la GPU

--- prueba.py
+++
@@ -1,4 +0,0 @@
-import torch
-print(torch.cuda.is_available())  # Debe imprimir True
-print(torch.cuda.device_count())  # Debe mostrar el número de GPUs
-print(torch.cuda.get_device_name(0))  # Nombre de la GPU
